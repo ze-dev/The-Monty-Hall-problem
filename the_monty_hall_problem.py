a40da6b02a597906a65d7cf916689d53fec4c484
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2021-01-27 23:03:54 +0300

    Update the_monty_hall_problem.py

--- the_monty_hall_problem.py
+++ the_monty_hall_problem.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 # ================================  classes
 
@@ -32,7 +32,7 @@ class Director(Showmen):
         # случайно определим номер, за какой будет а/м, и пусть режиссер его запомнит
         self.car_place = random.randint(1, 3)
         # теперь поместим за дверь с выбранным номером а/м, за другие - коз ).
-        for n in range(1,4,1):
+        for n in range(1,4,1): # тут достаточно делать выбор просто из трех, этим можно пренебречь
             if n == self.car_place:
                 our_wall[n].prize = "Car"
                 our_wall[n].prize_int = 1
@@ -53,29 +53,45 @@ class Host(Showmen):
         super().__init__()
 
 
-class Player:         # ============================================ class Player  - не идет. отладка в testMHP3
+class Player:
     """Игрок. Выбирает, какую дверь открыть"""
     def __init__(self):
         self.selected_door = None
 
-    def choose_door(self, the_same_door = False):
-        # при Ложь по умолчанию всегда выбираем новую дверь
-        if not the_same_door:
-            seq = [1,2,3]
-            # если дверь была ранее выбрана - исключим ее номер из выбора
-            if not self.selected_door == None:
-                seq.remove(self.selected_door)
-            # ну и собственно сам выбор
-            self.selected_door = random.choice(seq)
-        # при Истина оставляем выбор на старой
-        return self.selected_door
+    def choose_door(self,attempt, the_same_door = False):
+        # далее поубирать лишние ретены
+        main_seq = [1, 2, 3]
+        seq = [x for x in range (main_seq[0],main_seq[-1]*100+1,1)]
+        # при первом выборе выбираем любую вообще
+        if attempt == 1:
+            self.selected_door = self.accuracy(random.choice(seq))
+            return self.selected_door
+        # при втором выборе: зависит выбирать ли ту же самую дверь
+        if attempt == 2:
+            # если ту же самую
+            if the_same_door:
+                return self.selected_door
+            # если надо другую
+            else:
+                seq.remove(self.selected_door)  # ; print(seq)
+                self.selected_door = self.accuracy(random.choice(seq))
+                return self.selected_door
+    @staticmethod
+    def accuracy(got_num):
+        if got_num > 200:
+            return 3
+        elif got_num > 100:
+            return 2
+        else:
+            return 1
+    @staticmethod #  короче из (которая будет) 1 созлать последовательность 1- 100, далее из 2х - 2-100
+    def seq(main_seq):
+        for elem in seq:
+            seq_cur = # детальная отладка в третьем тестовом
 
 
 
-
-
-
-class Prize:  # перенесу функционал в дверь
+class Prize:  # перенести функционал в дверь
     '''Приз. Может быть, чем угодно, хоть козой, хоть автомобилем
     Помещается за дверь режиссером'''
     name: object
@@ -92,7 +108,11 @@ class Prize:  # перенесу функционал в дверь
 
 class Camera:
     '''Камера. Беспристрастно фиксирует происходящее в студии. Может показать все разультаты'''
-    pass
+    def __init__(self):
+        self.first_choice = None
+        self.second_choice = None
+    def record(self,choice):
+        self.first_choice = choice
 
 # ------------------------------------ body
 
@@ -107,10 +127,21 @@ wall = dict([(x.number, x) for x in door_list])
 # создаем режиссера и ведущего
 drc = Director()
 hst = Host()
+camera = Camera()
+plr = Player()
 # помещаем режиссером за двери А/м и двух коз
 drc.place_prize(wall)
 # сообщаем ведущему, где а/м
 drc.inform(hst)
+# ведущий должен спросить у грока, что он выберет, и камера должна это зафиксировать
+
+---------------------------проектируенм дальнейшшее поведение товарищей в студии
+
+camera.record(hst.ask(player.choose_door(1, thesame = false)))) ^  1 - номер попытки/ответа, то_же - вибыраем ли старое значение или будеи новое
+
+
+camera.recosd(host.reask(plkayer/chekagain(2, the_same = True)))
+camers.record(drc.obyavlzyet_ghde)
 
 # - - - - - - - - - - - - - - - - - - output
 
